8ce7a9e64898e770ef1754c5d60e7cae7fec5d69
Mehmet Emin Tiryaki <tiryakieminmehmet@gmail.com>
2023-07-22 21:35:12 +0300

    project initializer

---
+++ src/main/java/com/emin/couriercase/CouriercaseApplication.java
@@ -0,0 +1,13 @@
+package com.emin.couriercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouriercaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouriercaseApplication.class, args);
+	}
+
+}
